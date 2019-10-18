491914ab49f7f6b46180fdec7ee3087322ef1fc0
Maciej Kryczka <maciejkryczka@hotmail.com>
2019-10-18 11:35:21 +0200

    add OrderOption new components

--- src/components/features/OrderOption/OrderOption.js
+++ src/components/features/OrderOption/OrderOption.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import styles from './OrderOption.scss';
+import OrderOptionDropdown from './OrderOptionDropdown';
+import OrderOptionNumber from './OrderOptionNumber';
+import OrderOptionIcons from './OrderOptionIcons';
+import OrderOptionCheckboxes from './OrderOptionCheckboxes';
 
-const OrderOption = (option) => {
-  return(
-    <h3 className={styles.title}>{option.name}</h3>
-  );
+const optionTypes = {
+  dropdown: OrderOptionDropdown,
+  icons: OrderOptionIcons,
+  checkboxes: OrderOptionCheckboxes,
+  number: OrderOptionNumber,
+};
+
+const OrderOption = ({name, type, ...otherProps}) => {
+  const OptionComponent = optionTypes[type];
+  if(!OptionComponent){
+    return null;
+  } else{
+    return(
+      <div className={styles.component}>
+        <h3 className={styles.title}>{name}</h3>
+        <OptionComponent {...otherProps} />
+      </div>
+    );
+  }
 };
 
 export default OrderOption;
