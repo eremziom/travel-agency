db97d9127af6a8ff7ce941057564492f92b22c66
Maciej Kryczka <maciejkryczka@hotmail.com>
2019-10-29 14:29:48 +0100

    add json server and sendOrder function

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,6 +4,39 @@ import OrderSummary from '../OrderSummary/OrderSummary';
 import PropTypes from 'prop-types';
 import pricingArray from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
+
+const sendOrder = (options, tripCost) => {
+  console.log('options: ', options, ' tripCost: ', tripCost);
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+  console.log('options: ', options, ' tripCost: ', tripCost);
+
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 class OrderForm extends React.Component {
 
@@ -25,7 +58,7 @@ class OrderForm extends React.Component {
         ))}
         <Col xs={12}>
           <OrderSummary cost={tripCost} options={options}/>
-
+          <Button onClick={() => sendOrder(options, tripCost)}>Order now!</Button>
         </Col>
       </Row>
     );
