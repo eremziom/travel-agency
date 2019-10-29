41ccd73fd1760374b6d9c3a28cd8b6103bc90e7d
Maciej Kryczka <maciejkryczka@hotmail.com>
2019-10-29 19:54:33 +0100

    add name and contact inputs validation

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -10,33 +10,35 @@ import { calculateTotal } from '../../../utils/calculateTotal';
 import settings from '../../../data/settings';
 
 const sendOrder = (options, tripCost, tripName, countryCode) => {
-  console.log('options: ', options, ' tripCost: ', tripCost, 'name: ', tripName, 'code: ', countryCode);
-  const totalCost = formatPrice(calculateTotal(tripCost, options));
+  if(options.name && options.contact != '') {
+    console.log('options: ', options, ' tripCost: ', tripCost, 'name: ', tripName, 'code: ', countryCode);
+    const totalCost = formatPrice(calculateTotal(tripCost, options));
 
-  const payload = {
-    ...options,
-    totalCost,
-    tripName,
-    countryCode,
-  };
+    const payload = {
+      ...options,
+      totalCost,
+      tripName,
+      countryCode,
+    };
 
-  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+    const url = settings.db.url + '/' + settings.db.endpoint.orders;
 
-  const fetchOptions = {
-    cache: 'no-cache',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(payload),
-  };
+    const fetchOptions = {
+      cache: 'no-cache',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
 
-  fetch(url, fetchOptions)
-    .then(function(response){
-      return response.json();
-    }).then(function(parsedResponse){
-      console.log('parsedResponse', parsedResponse);
-    });
+    fetch(url, fetchOptions)
+      .then(function(response){
+        return response.json();
+      }).then(function(parsedResponse){
+        console.log('parsedResponse', parsedResponse);
+      });
+  }
 };
 
 class OrderForm extends React.Component {
