fef0efad4b035d6e5e52e905d7eabfbd9f670739
Maciej Kryczka <maciejkryczka@hotmail.com>
2019-10-31 22:17:06 +0100

    add mocked date test to HappyHourAd

---
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import HappyHourAd from './HappyHourAd';
+
+const select = {
+  title: '.title',
+  promoDescription: '.promoDescription',
+};
+
+const mockProps = {
+  title: 'sample title',
+  promoDescription: 'sample date or text',
+};
+
+describe('Component HappyHourAd', () => {
+
+  it('should render component without error', () => {
+    const component = shallow(<HappyHourAd />);
+    expect(component).toBeTruthy();
+  });
+
+  it('should render correctly both elements', () => {
+    const component = shallow(<HappyHourAd />);
+    expect(component.exists(select.title)).toEqual(true);
+    expect(component.exists(select.promoDescription)).toEqual(true);
+  });
+
+  it('should render correct title', () => {
+    const component = shallow(<HappyHourAd {...mockProps} />);
+    expect(component.find(select.title).text()).toEqual(mockProps.title);
+  });
+});
+
+const trueDate = Date;
+const mockDate = (customDate) => class extends Date {
+  constructor(...args) {
+    if(args.length){
+      super(...args);
+    } else {
+      super(customDate);
+    }
+    return this;
+  }
+  static now(){
+    return (new Date(customDate)).getTime();
+  }
+};
+
+const checkDescriptionAtTime = (time, expectedDescription) => {
+  it(`should show correct at ${time}`, () => {
+    global.Date = mockDate(`2019-05-15T${time}.135Z`);
+
+    const component = shallow(<HappyHourAd {...mockProps} />);
+    const renderedTime = component.find(select.promoDescription).text();
+    expect(renderedTime).toEqual(expectedDescription);
+
+    global.Date = trueDate;
+  });
+};
+
+describe('Component HappyHourAd with mocked Date', () => {
+  checkDescriptionAtTime('11:57:58', '122');
+  checkDescriptionAtTime('11:59:59', '1');
+  checkDescriptionAtTime('13:00:00', 23*60*60 + '');
+});
