6f31ec162a42a73e568237e6104706d5cb43b106
Maciej Kryczka <maciejkryczka@hotmail.com>
2019-11-08 09:51:55 +0100

    fix showContact function

---
+++ src/data/contact.js
@@ -0,0 +1,17 @@
+export const data = {
+  morning: {
+    dayPart: '08:00-12:00',
+    name: 'Amanda',
+    number: '678.243.8455',
+  },
+  afternoon: {
+    dayPart: '12:00-16:00',
+    name: 'Tobias',
+    number: '278.443.6443',
+  },
+  evening: {
+    dayPart: '16:00-22:00',
+    name: 'Helena',
+    number: '167.280.3970',
+  },
+};
--- src/utils/showContact.js
+++ src/utils/showContact.js
@@ -1,53 +1,37 @@
 import React from 'react';
 import styles from '../components/features/Contact/Contact.scss';
+import { data } from '../data/contact';
 
-export const showContact = dateArgument => {
-  const data = {
-    morning: {
-      dayPart: '08:00-12:00',
-      name: 'Amanda',
-      number: '678.243.8455',
-    },
-    afternoon: {
-      dayPart: '12:00-16:00',
-      name: 'Tobias',
-      number: '278.443.6443',
-    },
-    evening: {
-      dayPart: '16:00-22:00',
-      name: 'Helena',
-      number: '167.280.3970',
-    },
-  };
+export const showContact = date => {
 
-  if(dateArgument>=8 && dateArgument<12){
-    return(
-      <span>
-        <span className={styles.dayPart}>{data.morning.dayPart}</span>
-        <span className={styles.name}>{data.morning.name}</span>
-        <span className={styles.number}>{data.morning.number}</span>
-      </span>
-    );
-  } else if(dateArgument>=12 && dateArgument<16){
-    return(
-      <span>
-        <span className={styles.dayPart}>{data.afternoon.dayPart}</span>
-        <span className={styles.name}>{data.afternoon.name}</span>
-        <span className={styles.number}>{data.afternoon.number}</span>
-      </span>
-    );
-  } else if(dateArgument>=16 && dateArgument<22){
-    return(
-      <span>
-        <span className={styles.dayPart}>{data.evening.dayPart}</span>
-        <span className={styles.name}>{data.evening.name}</span>
-        <span className={styles.number}>{data.evening.number}</span>
-      </span>
-    );
-  } else
+  const contactTemplate = renderContact(date);
+
+  return contactTemplate;
+};
+
+const renderContact = date => {
+  if(date >= 22 && date < 8){
     return(
       <div>
         <span>Open Hours: 08:00-22:00</span>
       </div>
     );
+  }
+
+  let contactData = data.morning;
+
+  if(date >= 12 && date < 16){
+    contactData = date.afternoon;
+  }
+
+  if(date >= 16 && date < 22){
+    contactData = date.evening;
+  }
+  return(
+    <span>
+      <span className={styles.dayPart}>{contactData.dayPart}</span>
+      <span className={styles.name}>{contactData.name}</span>
+      <span className={styles.number}>{contactData.number}</span>
+    </span>
+  );
 };
